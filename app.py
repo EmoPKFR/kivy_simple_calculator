7577acdc78a6fab88e0fc37678c3158e09d27342
unknown <popunyak_123@abv.bg>
2023-11-21 00:30:47 +0200

    It is almost working except you cannot do multiple dots if the last sign is not +

--- app.py
+++ app.py
@@ -14,6 +14,65 @@ class MyLayout(Widget):
     def clear(self):
         self.ids.calc_input.text = "0"
         
+    #Create a button pressing function
+    def button_press(self, button):
+        #create a variable that contains whatever was in the text box already
+        prior = self.ids.calc_input.text
+        
+        #Test for error first
+        if "Error" in prior:
+            prior = ""
+        
+        #determine if 0 is sitting there
+        if prior == "0":
+            self.ids.calc_input.text = ""
+            self.ids.calc_input.text = f"{button}"
+        else:
+            self.ids.calc_input.text = f"{prior}{button}"
+            
+    def pos_neg(self):
+        prior = self.ids.calc_input.text
+        
+        if "-" in prior:
+            self.ids.calc_input.text = f"{prior.replace('-', '')}"
+        else:
+            self.ids.calc_input.text = f"-{prior}"          
+            
+    def remove(self):
+        prior = self.ids.calc_input.text
+        prior = prior[:-1]
+        self.ids.calc_input.text = prior
+            
+    # Create decimal function
+    def dot(self):
+        prior = self.ids.calc_input.text
+        num_list = prior.split("+")
+        
+        if "+" in prior and "." not in num_list[-1]:
+            prior = f"{prior}."
+            self.ids.calc_input.text = prior
+        elif "." in prior:
+            pass
+        else:       
+            prior = f"{prior}."
+            self.ids.calc_input.text = prior
+            
+            
+    def math_sign(self, sign):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f"{prior}{sign}"
+      
+    def equals(self):
+        prior = self.ids.calc_input.text
+        #Error handling
+        try:
+            #Evaluate the math from the text box
+            answer = eval(prior)
+            self.ids.calc_input.text = str(answer)
+        except:
+            self.ids.calc_input.text = "Error"
+            
+        
 class CalculatorApp(App):
     def build(self):
         return MyLayout()
